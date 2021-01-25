ee72dd0933ee4dcf16d9cbd3067da27897ff6bef
andi-s0106 <66274333+andi-s0106@users.noreply.github.com>
2021-01-25 09:08:39 -0500

    Update and rename 3-write_file.py to 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+	Writes a string to a text file
+'''
+
+
 def write_file(filename="", text=""):
+	'''
+		Writes a string to a text file
+	'''
     with open(filename, mode="w") as fd:
         nbc = fd.write(text)
     return nbc
