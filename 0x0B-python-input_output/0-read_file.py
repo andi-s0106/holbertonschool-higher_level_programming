40a2b813884c0a232370397a26ab0ea06314ab6e
andi-s0106 <66274333+andi-s0106@users.noreply.github.com>
2021-01-25 09:04:58 -0500

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,11 @@
 '''
         Reads the contents of a file
 '''
+
+
 def read_file(filename=""):
     '''
         Reads the contents of a file
     '''
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
