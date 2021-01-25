5bf32c529e99b1ae8e1168d0617d12166ade8db2
andi-s0106 <66274333+andi-s0106@users.noreply.github.com>
2021-01-25 09:36:55 -0500

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''
+	adds items to a json file
+	@args: The arguments that need to be added.
+	@filename: The file that needs to be updated
+'''
+
+
 import sys
 save_to_json = __import__("7-save_to_json_file").save_to_json_file
 load_from_json = __import__("8-load_from_json_file").load_from_json_file
