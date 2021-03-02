fab44db1116e539f740423598a1d4adb6a3e72e8
andi-s0106 <andress.933110@gmail.com>
2021-03-02 21:48:20 +0000

    updated 13-count_show_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_shows FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER by number_shows DESC;
+ORDER by number_shows DESC, genre;
