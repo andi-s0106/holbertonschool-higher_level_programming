53099ca8e5d94b7d539cc5df3af32d0a3c8165ad
andi-s0106 <andress.933110@gmail.com>
2021-04-10 02:24:17 +0000

    added 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+    takes in a URL and an email, sends a POST request to the passed URL with
+    the email as a parameter, and displays the body of the response
+'''
+import sys
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urlencode(values)
+    data = data.encode('ascii')
+    req = Request(url, data)
+
+    with urlopen(req) as request:
+        response = request.read()
+    print(response.decode("utf-8"))
